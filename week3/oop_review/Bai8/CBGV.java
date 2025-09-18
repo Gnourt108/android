940ce39c88f17bcab94cb3dca8a240afcdf612eb
gnourt108 <23115053122146@sv.ute.udn.vn>
2025-09-18 22:38:22 +0700

    Update CBGV.java

--- week3/oop_review/Bai8/CBGV.java
+++ week3/oop_review/Bai8/CBGV.java
@@ -1,5 +1,39 @@
 package Bai8;
 
-public class CBGV {
+import java.util.Scanner;
 
+class CBGV extends Nguoi {
+    private double luongCung_146;
+    private double thuong_146;
+    private double phat_146;
+    private double luongThucLinh_146;
+
+    public void nhap(Scanner sc) {
+        super.nhap(sc);
+        System.out.print("Nhap luong cung: ");
+        luongCung_146 = Double.parseDouble(sc.nextLine());
+        System.out.print("Nhap thuong: ");
+        thuong_146 = Double.parseDouble(sc.nextLine());
+        System.out.print("Nhap phat: ");
+        phat_146 = Double.parseDouble(sc.nextLine());
+
+        tinhLuongThucLinh();
+    }
+
+    private void tinhLuongThucLinh() {
+        luongThucLinh_146 = luongCung_146 + thuong_146 - phat_146;
+    }
+
+    public void hienThi() {
+        super.hienThi();
+        System.out.println("Luong cung: " + luongCung_146);
+        System.out.println("Thuong: " + thuong_146);
+        System.out.println("Phat: " + phat_146);
+        System.out.println("Luong thuc linh: " + luongThucLinh_146);
+        System.out.println("-----------------------");
+    }
+
+    public double getLuongThucLinh_146() {
+        return luongThucLinh_146;
+    }
 }
