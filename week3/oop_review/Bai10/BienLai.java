9282afcca6d3a5456f946687b7060f2d417dd7f9
gnourt108 <23115053122146@sv.ute.udn.vn>
2025-09-18 22:47:34 +0700

    Update BienLai.java

--- week3/oop_review/Bai10/BienLai.java
+++ week3/oop_review/Bai10/BienLai.java
@@ -1,5 +1,35 @@
 package Bai10;
 
-public class BienLai {
+import java.util.Scanner;
 
+class BienLai {
+    private KhachHang kh_146;
+    private int chiSoCu_146;
+    private int chiSoMoi_146;
+    private long soTienPhaiTra_146;
+
+    public void nhap(Scanner sc) {
+        kh_146 = new KhachHang();
+        System.out.println("Nhap thong tin khach hang");
+        kh_146.nhap(sc);
+
+        System.out.print("Nhap chi so cu: ");
+        chiSoCu_146 = Integer.parseInt(sc.nextLine());
+        System.out.print("Nhap chi so moi: ");
+        chiSoMoi_146 = Integer.parseInt(sc.nextLine());
+
+        tinhTienDien();
+    }
+
+    private void tinhTienDien() {
+        soTienPhaiTra_146 = (long)(chiSoMoi_146 - chiSoCu_146) * 750;
+    }
+
+    public void hienThi() {
+        kh_146.hienThi();
+        System.out.println("Chi so cu: " + chiSoCu_146);
+        System.out.println("Chi so moi: " + chiSoMoi_146);
+        System.out.println("So tien phai tra: " + soTienPhaiTra_146 + " VND");
+        System.out.println("----------------------------");
+    }
 }
