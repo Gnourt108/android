35ae359e673e30ac5a08ba7ab162ab0e23454953
gnourt108 <23115053122146@sv.ute.udn.vn>
2025-09-18 22:47:50 +0700

    Update KhachHang.java

--- week3/oop_review/Bai10/KhachHang.java
+++ week3/oop_review/Bai10/KhachHang.java
@@ -1,5 +1,24 @@
 package Bai10;
 
-public class KhachHang {
+import java.util.Scanner;
 
+class KhachHang {
+    private String hoTenChuHo_146;
+    private String soNha_146;
+    private String maSoCongTo_146;
+
+    public void nhap(Scanner sc) {
+        System.out.print("Nhap ho ten chu ho: ");
+        hoTenChuHo_146 = sc.nextLine();
+        System.out.print("Nhap so nha: ");
+        soNha_146 = sc.nextLine();
+        System.out.print("Nhap ma so cong to: ");
+        maSoCongTo_146 = sc.nextLine();
+    }
+
+    public void hienThi() {
+        System.out.println("Ho ten chu ho: " + hoTenChuHo_146);
+        System.out.println("So nha: " + soNha_146);
+        System.out.println("Ma so cong to: " + maSoCongTo_146);
+    }
 }
