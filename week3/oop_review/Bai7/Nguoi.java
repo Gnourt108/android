cb3d0206f127af412b77e0c02945dc8a54352aec
gnourt108 <23115053122146@sv.ute.udn.vn>
2025-09-18 22:27:38 +0700

    Update Nguoi.java

--- week3/oop_review/Bai7/Nguoi.java
+++ week3/oop_review/Bai7/Nguoi.java
@@ -1,5 +1,33 @@
 package Bai7;
 
-public class Nguoi {
+import java.util.Scanner;
 
+class Nguoi {
+    protected String hoTen_146;
+    protected String ngaySinh_146; 
+    protected String queQuan_146;
+
+    public void nhap(Scanner sc) {
+        System.out.print("Nhap ho ten: ");
+        hoTen_146 = sc.nextLine();
+        System.out.print("Nhap ngay sinh (dd/MM/yyyy): ");
+        ngaySinh_146 = sc.nextLine();
+        System.out.print("Nhap que quan: ");
+        queQuan_146 = sc.nextLine();
+    }
+
+    public void hienThi() {
+        System.out.println("Ho ten: " + hoTen_146);
+        System.out.println("Ngay sinh: " + ngaySinh_146);
+        System.out.println("Que quan: " + queQuan_146);
+    }
+
+    public int getNamSinh_146() {
+        String[] parts = ngaySinh_146.split("/");
+        return Integer.parseInt(parts[2]);
+    }
+
+    public String getQueQuan_146() {
+        return queQuan_146;
+    }
 }
