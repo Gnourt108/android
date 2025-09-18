39585a1d25a816e062b89697799dd7329ad9c83a
gnourt108 <23115053122146@sv.ute.udn.vn>
2025-09-18 22:38:05 +0700

    Update Nguoi.java

--- week3/oop_review/Bai8/Nguoi.java
+++ week3/oop_review/Bai8/Nguoi.java
@@ -1,5 +1,26 @@
-package Bai7;
+package Bai8;
 
-public class Nguoi {
+import java.util.Scanner;
 
+class Nguoi {
+    protected String hoTen_146;
+    protected String ngaySinh_146; 
+    protected String queQuan_146;
+
+    public void nhap(Scanner sc) {
+        System.out.print("Nhap ho ten: ");
+        hoTen_146 = sc.nextLine();
+        System.out.print("Nhap ngay sinh (dd/MM/yyyy): ");
+        ngaySinh_146 = sc.nextLine();
+        System.out.print("Nhap que quan: ");
+        queQuan_146 = sc.nextLine();
+    }
+
+    public void hienThi() {
+        System.out.println("Ho ten: " + hoTen_146);
+        System.out.println("Ngay sinh: " + ngaySinh_146);
+        System.out.println("Que quan: " + queQuan_146);
+    }
 }
+
+
